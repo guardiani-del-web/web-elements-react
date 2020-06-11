b02e1160a8f64ff12a74676c4584e67857f38024
FAngelo94 <a.falci@live.it>
2020-06-11 09:58:35 +0200

    added web-elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          React with Web Components
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/guardiani-del-web/web-elements-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Work in Progress
-        </a>
-      </header>
-    </div>
+    <we-tooltip value="simple tooltip">
+      <label slot="reference"> Put on this container to see the tooltip </label>
+    </we-tooltip>
   );
 }
 
