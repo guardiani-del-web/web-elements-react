cdb94ce7c8debbc874263a1230d86a2245234833
FAngelo94 <a.falci@live.it>
2020-06-11 09:14:05 +0200

    project initialize2

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React with Web Components
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/guardiani-del-web/web-elements-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Work in Progress
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
