cbeb627320400a60f29d9697542b91de7b3ff9c5
Angelo Falci <angelo.falci@external.fcagroup.com>
2020-06-23 16:56:19 +0200

    updated chips and switch

--- src/components/chips/index.jsx
+++ src/components/chips/index.jsx
@@ -2,17 +2,41 @@ import React from "react";
 import './../../App.css';
 
 function Chips() {
-    const srcLeft = "https://via.placeholder.com/15";
-    const srcRight = "https://via.placeholder.com/15";
-    const label = "label";
+    const nameGroup = 'chipsGroup';
+
     const removeLeft = true;
+
+    const isSelectable = true;
+
+    const image1 = 'https://via.placeholder.com/15';
+    const image2 = 'https://via.placeholder.com/15';
+    const label1 = 'label 1';
+    const label2 = 'label 2';
+    const label3 = 'label 3';
+
+    const value1 = 'chip 1';
+    const value2 = 'chip 2';
+    const value3 = 'chip 3';
+
+
+    const selectCallback = value => {
+        console.log('selectCallback', value);
+    }
+
+    const removeCallback = value => {
+        console.log('removeCallback', value);
+    }
 
     return (
         <div class="component">
             <h2>Chips</h2>
-            <we-chips src-img-left={srcLeft} src-img-right={srcRight} label={label} remove-left={removeLeft} />
+            <we-chips-group name={nameGroup} select-callback={selectCallback} remove-callback={removeCallback}>
+                <we-chips value={value1} src-img-left={image1} src-img-right={image2} label={label1} remove-left={removeLeft} is-selectable={isSelectable}></we-chips>
+                <we-chips value={value2} src-img-left={image1} src-img-right={image2} label={label2} remove-left={removeLeft} is-selectable={isSelectable}></we-chips>
+                <we-chips value={value3} src-img-left={image1} src-img-right={image2} label={label3} remove-left={removeLeft} is-selectable={isSelectable}></we-chips>
+            </we-chips-group>
         </div>
-    );
+    )
 }
 
 export default Chips;
--- src/components/switch/index.jsx
+++ src/components/switch/index.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import './../../App.css';
 
 function Switch() {
-    const name = 'MySwitch';
-  const enabled = false;
+  const name1 = 'MySwitch 1';
+  const name2 = 'MySwitch 2';
+  const name3 = 'MySwitch 3';
+  const checked = false;
+
+  const nameGroup = "Switch Group";
 
   const switchHandler = value => {
     console.log('switchHandler', value);
   }
 
-    return (
-        <div class="component">
-            <h2>Switch</h2>
-            <we-switch name={name} enabled={enabled} change-callback={switchHandler}></we-switch>
-        </div>
-    );
+  return (
+    <div class="component">
+      <h2>Switch</h2>
+      <we-switch-group name={nameGroup} change-switch-callback={switchHandler}>
+        <we-switch name={name1} checked={checked}></we-switch>
+        <we-switch name={name2} checked={checked}></we-switch>
+        <we-switch name={name3} checked={checked}></we-switch>
+      </we-switch-group>
+    </div>
+  );
 }
 
 export default Switch;
